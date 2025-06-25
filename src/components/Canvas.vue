5d615a29f6b440ff37b831cf25620865c048a727
TheDanDan <77023480+TheDanDan@users.noreply.github.com>
2025-06-25 17:26:30 -0400

    Created markboard component

---
+++ src/components/Canvas.vue
@@ -0,0 +1,81 @@
+<script setup>
+import { ref, onMounted, onUnmounted } from 'vue'
+
+const markboard = ref()
+let context = null
+
+const drawing = ref(false)
+const colour = ref('#000000')
+const width = ref(4)
+
+onMounted(() => {
+  const canvasElement = markboard.value
+  if (!canvasElement) {
+    return
+  }
+  context = canvasElement.getContext('2d')
+  if (!context) return
+
+  canvasElement.width = 1920
+  canvasElement.height = 1080
+
+  context.fillStyle = '#fff'
+  context.fillRect(0, 0, canvasElement.width, canvasElement.height)
+})
+
+onUnmounted(() => {
+  context = null
+})
+
+const startDrawing = (e) => {
+  if (!context) return
+
+  drawing.value = true
+
+  context.beginPath()
+  context.moveTo(e.offsetX, e.offsetY)
+}
+
+const paint = (e) => {
+  if (!drawing.value || !context) return
+
+  context.strokeStyle = colour.value
+  context.lineWidth = width.value
+
+  context.lineTo(e.offsetX, e.offsetY)
+  context.stroke()
+}
+
+const stopDrawing = () => {
+  if (!drawing.value || !context) return
+  
+  drawing.value = false
+  context.closePath()
+}
+</script>
+
+<template>
+  <div class="markboard-container">
+    <canvas
+      ref="markboard"
+      @mousedown="startDrawing"
+      @mousemove="paint"
+      @mouseup="stopDrawing"
+      @mouseleave="stopDrawing"
+    ></canvas>
+  </div>
+</template>
+
+<style scoped>
+.markboard-container {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+
+canvas {
+  border: 2px solid #000;
+  border-radius: 16px;
+  cursor: crosshair;
+}
+</style>
---
+++ src/components/ToolBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="toolbar">
+    <div class="toolbar-colours">
+      <label for="colour">Colour:</label>
+      <input type="color" id="colour" v-model="colour" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+const colour = ref('#000000')
+const width = ref(5)
+</script>
+
+<style scoped>
+.toolbar {
+  position: fixed;
+}
+</style>
