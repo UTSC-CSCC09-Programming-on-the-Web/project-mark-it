9e601266ff5d45785f6a155878c93162a2f0d98d
Xing Yu (Joe) Zhou <mr.joezhou@gmail.com>
2025-06-25 17:00:10 -0400

    model for user information

---
+++ server/models/user.js
@@ -0,0 +1,14 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../datasource.js";
+
+export const User = sequelize.define("User", {
+  googleId: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  displayName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
