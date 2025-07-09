9a8ffb28b37564b1c34f0ea7c4da464cfc0bb0a5
Danny <danny-987123@outlook.com>
2025-07-09 17:12:02 -0400

    feat: added socket server

---
+++ sockets/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import { createServer } from 'node:http';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  socket.on('paint', (msg) => {
+    console.log('paint: ' + msg);
+    socket.broadcast.emit('paint', msg);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('server running at http://localhost:3000');
+});
