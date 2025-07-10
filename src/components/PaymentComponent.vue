bad40c6d2478a83811d652c645a5a4515117eda9
Xing Yu (Joe) Zhou <mr.joezhou@gmail.com>
2025-07-09 22:15:20 -0400

    integrated checkout.html (needs some setup on stripe website)

---
+++ src/components/PaymentComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h3>Subscription to Market for $2.00 a month</h3>
+    <button @click="createCheckoutSession">Checkout</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    async createCheckoutSession() {
+      const response = await fetch('/api/payment/create-checkout-session', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ lookup_key: 'markit_monthly_subscription' })
+      })
+      const data = await response.json()
+      window.location.href = data.url
+    }
+  }
+}
+</script>
+
+
