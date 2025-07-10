1e1d34ed9b197620137184805200b95ed1587eae
Xing Yu (Joe) Zhou <mr.joezhou@gmail.com>
2025-07-09 22:16:26 -0400

    temporary success and cancel pages

---
+++ src/components/CancelPage.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <p>Please pay the subscription to use Markit!</p>
+  </div>
+</template>
+
+
---
+++ src/components/SuccessPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h3>Thanks for subscribing!</h3>
+    <button @click="createPortalSession">Manage your billing information</button>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.sessionId = new URLSearchParams(window.location.search).get('session_id')
+  },
+  methods: {
+    async createPortalSession() {
+      const response = await fetch('/api/payment/create-portal-session', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ session_id: this.sessionId })
+      })
+      const data = await response.json()
+      window.location.href = data.url
+    }
+  }
+}
+</script>
+
+
